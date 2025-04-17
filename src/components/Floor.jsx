7445bfec1c0b6c16fe0b57cfcd047a02f81ee52b
Jesús de la Fuente <jfuentelucas@gmail.com>
2025-04-17 17:27:49 +0200

    floor component improvement

--- src/components/Floor.jsx
+++ src/components/Floor.jsx
@@ -1,5 +1,5 @@
 import { useTexture } from "@react-three/drei";
-import { DoubleSide } from "three";
+import * as THREE from "three";
 
 const Floor = () => {
   const props = useTexture({
@@ -9,6 +9,28 @@ const Floor = () => {
     aoMap: "textures/coast_sand_rocks_02_arm_1k.webp",
     displacementMap: "textures/coast_sand_rocks_02_disp_1k.webp", // Descomentar para el mapa de desplazamiento
   });
+
+  // Acceder a las texturas
+  const { map, normalMap, roughnessMap, aoMap, displacementMap } = props;
+
+  map.repeat.set(8, 8);
+  normalMap.repeat.set(8, 8);
+  roughnessMap.repeat.set(8, 8);
+  displacementMap.repeat.set(8, 8);
+
+  // Establecer el modo de envolvimiento (wrap) de las texturas
+  map.wrapS =
+    normalMap.wrapS =
+    roughnessMap.wrapS =
+    displacementMap.wrapS =
+      THREE.RepeatWrapping;
+  map.wrapT =
+    normalMap.wrapT =
+    roughnessMap.wrapT =
+    displacementMap.wrapT =
+      THREE.RepeatWrapping;
+  // Establecer el espacio de color de la textura de color
+  map.colorSpace = THREE.SRGBColorSpace;
 
   return (
     <mesh
@@ -20,12 +42,14 @@ const Floor = () => {
       <planeGeometry args={[40, 40]} />
       <meshStandardMaterial
         {...props}
-        side={DoubleSide}
+        side={THREE.DoubleSide}
         displacementScale={0} // Ajustar según el mapa de desplazamiento
         roughness={0.8} // Ajustar rugosidad
         normalMap={props.normalMap}
         aoMap={props.aoMap}
         displacementMap={props.displacementMap}
+        displacementBias={0}
+        transparent
       />
     </mesh>
   );
